963cc47b2281d0d01d47a1ff73b7ac93929404e4
i518353 <leonhard.stengel@sap.com>
2022-03-23 14:41:36 +0100

    Added group chats functionality

--- application/server.py
+++ application/server.py
@@ -14,6 +14,14 @@ class Server:
         self.socket.listen(100)
         self.list_of_clients = []
         self.usernames = {}
+        self.rooms = {
+        }
+        self.command_dict = {
+            'lr': [self.list_rooms, 0],
+            'join': [self.join_room, 1],
+            'leave': [self.leave_room, 0],
+            'penis': [self.leave_room, 0]
+        }
         while True:
             
             connection, address = self.socket.accept()
@@ -24,6 +32,7 @@ class Server:
 
             self.list_of_clients.append(connection)
             self.usernames[connection] = username
+            self.rooms[connection] = 'lobby'
 
             start_new_thread(self.handle_clients,(connection,address)) 
 
@@ -32,23 +41,61 @@ class Server:
         
     def handle_clients(self,connection, address):
         connection.send("Willkommen im Chat".encode())
+        connection.send("Commands: \n /lr - List rooms \n /join - Join Room \n /leave - Leave room".encode())
         while True:
             try: 
-                message = connection.recv(1024)
+                message = connection.recv(1024).decode()
                 if message:
-                    self.broadcast(message, connection)
+                    if str(message).startswith('/'):
+                        self.handle_command(message, connection)
+                    else:
+                        username = self.usernames[connection]
+                        self.broadcast(username + ': ' + message, connection)
                 else:
                     self.remove(connection)
-            except:
+            except Exception as e:
+
+                print(e)
                 continue
 
-    def broadcast(self,message, connection):
-        for clients in self.list_of_clients:
-            if clients!=connection:
+    def handle_command(self, message, connection):
+        tokens = message.split()
+        command = tokens[0][1:]
+        args = tokens[1:]
+
+        if not command in self.command_dict:
+            print('Command not found')
+        else:
+            cmd = self.command_dict[command]
+            if len(args) != cmd[1]:
+                print('Wrong number of arguments')
+            elif len(args) == 0:
+                cmd[0](connection)
+            else:
+                cmd[0](connection, args)
+    
+    def list_rooms(self, connection):
+        rooms_list = ' '.join([r for r in set(self.rooms.values())])
+        connection.send(rooms_list.encode())
+
+    def join_room(self, connection, args):
+        self.rooms[connection] = args[0]
+        connection.send((f'Welcome in Room {args[0]}').encode())
+
+
+    def leave_room(self, connection):
+        self.rooms[connection] = 'lobby'
+        connection.send((f'Welcome in the Lobby').encode())
+
+
+
+    def broadcast(self, message, connection):
+        for client in self.list_of_clients:
+            if client!=connection and self.rooms[connection] == self.rooms[client]:
                 try:
-                    clients.send(message)
+                    client.send(message.encode())
                 except:
-                    clients.close()
+                    client.close()
                     self.remove(clients) 
 
     def remove(self,connection):
